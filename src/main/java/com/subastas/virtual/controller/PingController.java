7da6c5c599eb684dc820da4e7e9c0b64ec0f0730
fgeorgescu <franco.georgescu@mercadolibre.com>
2021-05-16 16:41:33 -0300

    add crossOrigin

--- src/main/java/com/subastas/virtual/controller/PingController.java
+++ src/main/java/com/subastas/virtual/controller/PingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 
 @RestController
+@CrossOrigin
 public class PingController {
 
     Logger log = LoggerFactory.getLogger(PingController.class);
--- src/main/java/com/subastas/virtual/controller/SessionController.java
+++ src/main/java/com/subastas/virtual/controller/SessionController.java
@@ -8,14 +8,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
-@Controller
+@RestController
+@CrossOrigin
 public class SessionController {
 
     Logger log = LoggerFactory.getLogger(SessionController.class);
