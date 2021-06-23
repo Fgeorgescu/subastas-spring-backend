0f7420e61875138578e72860fce5f4e30fdb2076
fgeorgescu <franco.georgescu@mercadolibre.com>
2021-06-23 20:00:48 -0300

    add subastas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,22 +11,78 @@ INSERT INTO items (id, title, description, auction, status, owner)
     VALUES (1, 'Macbook Pro 2018', 'Macbook pro 2018 con poco uso y mucho cuidado', 1, 'PROCESSING', 1);
 
 INSERT INTO items (id, title, description, auction, status, owner)
-VALUES (2, 'Macbook Pro 2018 - 2', 'Macbook pro 2018 con poco uso y mucho cuidado', 2, 'PROCESSING', 3);
+    VALUES (2, 'Calzado montaña', 'Calzado robusto para hacer montañismo', 2, 'PROCESSING', 2);
+
+INSERT INTO items (id, title, description, auction, status, owner)
+    VALUES (3, 'Calzado deportivo Nike', 'Botines para deportes varios en superficie de pasto', 2, 'PROCESSING', 3);
+
+INSERT INTO items (id, title, description, auction, status, owner)
+    VALUES (4, 'Zapatilla Basquet', 'Calzado deportivo para deportes como básquet o voley', 2, 'PROCESSING', 1);
+
+INSERT INTO items (id, title, description, auction, status, owner)
+    VALUES (5, 'Altas llantas', 'Las llantas más altas del mercado', 2, 'PROCESSING', 1);
+
+INSERT INTO items (id, title, description, auction, status, owner)
+    VALUES (6, 'Pelota clásica', 'Pelota clásica de futbol', 2, 'PROCESSING', 2);
+
+INSERT INTO items (id, title, description, auction, status, owner)
+    VALUES (7, 'Juego de te', 'Colección de tetera y copas + platos para tomar el te', 3, 'PROCESSING', 1);
+
+INSERT INTO items (id, title, description, auction, status, owner)
+    VALUES (8, 'Masajeador', 'Masajeador de hombros y espalda. Muy versátil', 3, 'PROCESSING', 3);
+
+INSERT INTO items (id, title, description, auction, status, owner)
+    VALUES (9, 'Plancha de pelo', 'Electrodomestico para planchado de pelo', 3, 'PROCESSING', 1);
+
+INSERT INTO items (id, title, description, auction, status, owner)
+    VALUES (10, 'Celular random', 'Celular en perfecto estado. Caracteristidas: \n probamos un \\n', 3, 'PROCESSING', 2);
 
 -- Insert photos linked to item
 INSERT INTO image_urls (registered_item_id, image_urls)
-values (1, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624218387/initial/macbook-pro_1.jpg');
+    values (1, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624218387/initial/macbook-pro_1.jpg');
 INSERT INTO image_urls (registered_item_id, image_urls)
-values (1, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624218386/initial/macbook-pro_2.jpg');
+    values (1, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624218386/initial/macbook-pro_2.jpg');
 
 INSERT INTO image_urls (registered_item_id, image_urls)
-values (2, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624218387/initial/macbook-pro_1.jpg');
+    values (2, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624488419/initial/zapatilla_4.jpg');
+
 INSERT INTO image_urls (registered_item_id, image_urls)
-values (2, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624218386/initial/macbook-pro_2.jpg');
+values (3, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624488419/initial/zapatilla_2.jpg');
+
+INSERT INTO image_urls (registered_item_id, image_urls)
+values (4, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624488419/initial/zapatilla_3.jpg');
+
+INSERT INTO image_urls (registered_item_id, image_urls)
+values (5, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624488419/initial/zapatilla_1.jpg');
+
+INSERT INTO image_urls (registered_item_id, image_urls)
+values (6, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624488442/initial/pelota-futbol.jpg');
+
+INSERT INTO image_urls (registered_item_id, image_urls)
+values (7, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624488481/initial/juego-te_2.jpg');
+
+INSERT INTO image_urls (registered_item_id, image_urls)
+values (7, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624488481/initial/juego-te_1.jpg');
+
+INSERT INTO image_urls (registered_item_id, image_urls)
+values (8, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624488534/initial/masajeador.jpg');
+
+INSERT INTO image_urls (registered_item_id, image_urls)
+values (9, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624488500/initial/plancha-pelo.jpg');
+
+INSERT INTO image_urls (registered_item_id, image_urls)
+values (10, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624488549/initial/celular_2.jpg');
+
+INSERT INTO image_urls (registered_item_id, image_urls)
+values (10, 'https://res.cloudinary.com/dr4i78wvu/image/upload/v1624488562/initial/celular_1.jpg');
 
 -- Insert auctions
 INSERT INTO auction (id, title, category, status)
     VALUES (1, 'Subasta tecno', 'CLASSIC', 'PENDING');
 
 INSERT INTO auction (id, title, category, status)
-VALUES (2, 'Subasta tecno - 2', 'GOLD', 'PENDING');
+VALUES (2, 'Deportes', 'GOLD', 'PENDING');
+
+
+INSERT INTO auction (id, title, category, status)
+VALUES (3, 'Cosas para el hogar', 'Silver', 'PENDING');
